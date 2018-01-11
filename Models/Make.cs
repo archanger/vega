05d6d7e520f87167b19fd6e6cd14f94080b01032
Kirill Chuyanov <kirill.chuyanov@lazada.com>
2018-01-11 15:26:19 +0300

    populate db

--- Models/Make.cs
+++ Models/Make.cs
@@ -1,16 +1,19 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Models
 {
     public class Make
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; } 
 
         public Make()
         {
-            Models = new Collection<Model>();
+            Models = new System.Collections.ObjectModel.Collection<Model>();
         }
     }
 }
